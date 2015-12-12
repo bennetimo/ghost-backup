ebf3257b49f5cb6c2a37ff858361987cf020ea1d
Tim Bennett <bennetimo@gmail.com>
2015-12-12 01:25:19 +0000

    add mysqldump backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Backing up Ghost files to $BACKUP_LOCATION"
-tar cvfz $BACKUP_LOCATION/backup_`date '+%Y%m%d'`.tar.gz /var/lib/ghost
+# If container has been linked correctly, these environment variables should be available
+if [ -z "$MYSQL_ENV_MYSQL_USER" ]; then echo "Error: MYSQL_ENV_MYSQL_USER not set. Have you linked in the mysql/mariadb container?"; echo "Finished: FAILURE"; exit 1; fi
+if [ -z "$MYSQL_ENV_MYSQL_DATABASE" ]; then echo "Error: MYSQL_ENV_MYSQL_DATABASE not set. Have you linked in the mysql/mariadb container?"; echo "Finished: FAILURE"; exit 1; fi
+if [ -z "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]; then echo "Error: MYSQL_ENV_MYSQL_PASSWORD not set. Have you linked in the mysql/mariadb container?"; echo "Finished: FAILURE"; exit 1; fi
 
-echo "Backup complete"
+echo "Creating ghost files archive"
+tar cvfz $BACKUP_LOCATION/backup-ghost_`date '+%Y%m%d'`.tar.gz /var/lib/ghost
+
+echo "Creating database dump"
+mysqldump -h mysql --single-transaction -u $MYSQL_ENV_MYSQL_USER --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD $MYSQL_ENV_MYSQL_DATABASE | 
+ gzip -c > $BACKUP_LOCATION/backup-db_`date '+%Y%m%d'`.sql.gz
+
+echo "Completed backup to $BACKUP_LOCATION"
