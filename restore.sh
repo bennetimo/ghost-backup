dd42c059083021c45d294cba6c13bd4187c84f1d
Tim Bennett <bennetimo@gmail.com>
2015-12-14 14:54:38 +0000

    add non-interactive restore

--- restore.sh
+++ restore.sh
@@ -7,6 +7,8 @@ DB_ARCHIVE_MATCH='db'
 # Ghost files location
 GHOST_LOCATION='/var/lib/ghost'
 
+usage() { echo "Usage: ghost-restore [-i (interactive)] [-d <yyyymmdd>]" 1>&2; exit 1; }
+
 # Restore the database from the given archive file
 restoreDB () {
 	RESTORE_FILE=$1
@@ -34,30 +36,61 @@ chooseFile () {
 		[[ -z $FILENAME ]] && choice=$REPLY || choice=$FILENAME
 	  case $choice in
 	  		q|Q|exit) 
-				break;
-				;;
-	        *)
-				if [[ $choice =~ .*$DB_ARCHIVE_MATCH.* ]]
-				then
-					restoreDB $choice
-				elif [[ $choice =~ .*$GHOST_ARCHIVE_MATCH.* ]]
-				then
-					restoreGhost $choice
-				else
-					echo "unrecognised format - the file should be either a ghost files or db archive"
-				fi
-	          ;;
+  				break;
+  				;;
+        *)
+  				if [[ $choice =~ .*$DB_ARCHIVE_MATCH.* ]]; then
+  					restoreDB $choice
+  				elif [[ $choice =~ .*$GHOST_ARCHIVE_MATCH.* ]]; then
+  					restoreGhost $choice
+  				else
+  					echo "unrecognised format - the file should be either a ghost files or db archive"
+  				fi
+          ;;
+        \?)
+          echo "usage..."
+          ;;
 	  esac
 	done
 }
 
-while getopts "i" opt; do
+restoreDate () {
+  DATE=$1
+  GHOST_ARCHIVE="$BACKUP_LOCATION/backup-ghost_$DATE.tar.gz"
+  DB_ARCHIVE="$BACKUP_LOCATION/backup-db_$DATE.sql.gz"
+
+  if [ ! -f $GHOST_ARCHIVE ]; then
+      echo "The ghost archive file $GHOST_ARCHIVE does not exist. Aborting."
+      exit 1
+  fi
+  if [ ! -f $DB_ARCHIVE ]; then
+      echo "The ghost db archive file $DB_ARCHIVE does not exist. Aborting."
+      exit 1
+  fi
+
+  echo "Restoring ghost files and db from date: $DATE"
+  restoreGhost $GHOST_ARCHIVE
+  restoreDB $DB_ARCHIVE
+}
+
+while getopts "id:" opt; do
 	case $opt in
 		i)
 			chooseFile
+      exit 0
 			;;
+    d)
+      restoreDate ${OPTARG}
+      exit 0
+      ;;
+    \?)
+      usage
+      exit 0
+      ;;
 	esac
 done
+
+usage
 
 
 
