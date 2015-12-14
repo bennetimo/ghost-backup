49e0de2887a5d9f648488693491320e53c2610d1
Tim Bennett <bennetimo@gmail.com>
2015-12-14 14:57:13 +0000

    fix formatting

--- restore.sh
+++ restore.sh
@@ -11,47 +11,47 @@ usage() { echo "Usage: ghost-restore [-i (interactive)] [-d <yyyymmdd>]" 1>&2; e
 
 # Restore the database from the given archive file
 restoreDB () {
-	RESTORE_FILE=$1
-	echo "restoring data from mysql dump file: $RESTORE_FILE"
-	gunzip < $RESTORE_FILE | mysql -u$MYSQL_ENV_MYSQL_USER -p $MYSQL_ENV_MYSQL_DATABASE -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql 
-	echo "restore complete"
+  RESTORE_FILE=$1
+  echo "restoring data from mysql dump file: $RESTORE_FILE"
+  gunzip < $RESTORE_FILE | mysql -u$MYSQL_ENV_MYSQL_USER -p $MYSQL_ENV_MYSQL_DATABASE -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql 
+  echo "restore complete"
 }
 
 restoreGhost () {
-	RESTORE_FILE=$1
-	echo "removing ghost files in $GHOST_LOCATION"
-	rm -r $GHOST_LOCATION/*
-	echo "restoring ghost files from archive file: $RESTORE_FILE"
-	tar -xzf $RESTORE_FILE --directory=$GHOST_LOCATION
-	echo "restore complete"
+  RESTORE_FILE=$1
+  echo "removing ghost files in $GHOST_LOCATION"
+  rm -r $GHOST_LOCATION/*
+  echo "restoring ghost files from archive file: $RESTORE_FILE"
+  tar -xzf $RESTORE_FILE --directory=$GHOST_LOCATION
+  echo "restore complete"
 }
 
 # Interactively choose a DB or ghost files archive to restore
 chooseFile () {
-	echo "Select DB or Ghost archive file to restore, or 'q' to quit"
-	PS3="Restore #: "
+  echo "Select DB or Ghost archive file to restore, or 'q' to quit"
+  PS3="Restore #: "
 
-	select FILENAME in $BACKUP_LOCATION/*;
-	do
-		[[ -z $FILENAME ]] && choice=$REPLY || choice=$FILENAME
-	  case $choice in
-	  		q|Q|exit) 
-  				break;
-  				;;
+  select FILENAME in $BACKUP_LOCATION/*;
+  do
+    [[ -z $FILENAME ]] && choice=$REPLY || choice=$FILENAME
+    case $choice in
+        q|Q|exit) 
+          break;
+          ;;
         *)
-  				if [[ $choice =~ .*$DB_ARCHIVE_MATCH.* ]]; then
-  					restoreDB $choice
-  				elif [[ $choice =~ .*$GHOST_ARCHIVE_MATCH.* ]]; then
-  					restoreGhost $choice
-  				else
-  					echo "unrecognised format - the file should be either a ghost files or db archive"
-  				fi
+          if [[ $choice =~ .*$DB_ARCHIVE_MATCH.* ]]; then
+            restoreDB $choice
+          elif [[ $choice =~ .*$GHOST_ARCHIVE_MATCH.* ]]; then
+            restoreGhost $choice
+          else
+            echo "unrecognised format - the file should be either a ghost files or db archive"
+          fi
           ;;
         \?)
           echo "usage..."
           ;;
-	  esac
-	done
+    esac
+  done
 }
 
 restoreDate () {
@@ -74,11 +74,11 @@ restoreDate () {
 }
 
 while getopts "id:" opt; do
-	case $opt in
-		i)
-			chooseFile
+  case $opt in
+    i)
+      chooseFile
       exit 0
-			;;
+      ;;
     d)
       restoreDate ${OPTARG}
       exit 0
@@ -87,7 +87,7 @@ while getopts "id:" opt; do
       usage
       exit 0
       ;;
-	esac
+  esac
 done
 
 usage
