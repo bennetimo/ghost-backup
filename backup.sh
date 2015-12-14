cfb845f596a93ede57dd05f425cd5f216e446ae9
Tim Bennett <bennetimo@gmail.com>
2015-12-14 14:07:47 +0000

    add restore of ghost files

--- backup.sh
+++ backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Ghost files location
+GHOST_LOCATION='/var/lib/ghost'
+
 # If container has been linked correctly, these environment variables should be available
 if [ -z "$MYSQL_ENV_MYSQL_USER" ]; then echo "Error: MYSQL_ENV_MYSQL_USER not set. Have you linked in the mysql/mariadb container?"; echo "Finished: FAILURE"; exit 1; fi
 if [ -z "$MYSQL_ENV_MYSQL_DATABASE" ]; then echo "Error: MYSQL_ENV_MYSQL_DATABASE not set. Have you linked in the mysql/mariadb container?"; echo "Finished: FAILURE"; exit 1; fi
 if [ -z "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]; then echo "Error: MYSQL_ENV_MYSQL_PASSWORD not set. Have you linked in the mysql/mariadb container?"; echo "Finished: FAILURE"; exit 1; fi
 
 echo "Creating ghost files archive"
-tar cvfz $BACKUP_LOCATION/backup-ghost_`date '+%Y%m%d'`.tar.gz /var/lib/ghost
+tar cvfz "$BACKUP_LOCATION/backup-ghost_`date '+%Y%m%d'`.tar.gz" --directory=$GHOST_LOCATION .
 
 echo "Creating database dump"
 mysqldump -h mysql --single-transaction -u $MYSQL_ENV_MYSQL_USER --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD $MYSQL_ENV_MYSQL_DATABASE | 
--- restore.sh
+++ restore.sh
@@ -1,24 +1,63 @@
 #!/bin/bash
 
-restore () {
+# Match string to indicate a ghost archive
+GHOST_ARCHIVE_MATCH='ghost'
+# Match string to indicate a db archive
+DB_ARCHIVE_MATCH='db'
+# Ghost files location
+GHOST_LOCATION='/var/lib/ghost'
+
+# Restore the database from the given archive file
+restoreDB () {
 	RESTORE_FILE=$1
-	echo "restoring data from mysql dump file $RESTORE_FILE"
+	echo "restoring data from mysql dump file: $RESTORE_FILE"
 	gunzip < $RESTORE_FILE | mysql -u$MYSQL_ENV_MYSQL_USER -p $MYSQL_ENV_MYSQL_DATABASE -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql 
 	echo "restore complete"
 }
 
-echo "Select file to restore"
+restoreGhost () {
+	RESTORE_FILE=$1
+	echo "removing ghost files in $GHOST_LOCATION"
+	rm -r $GHOST_LOCATION/*
+	echo "restoring ghost files from archive file: $RESTORE_FILE"
+	tar -xzf $RESTORE_FILE --directory=$GHOST_LOCATION
+	echo "restore complete"
+}
 
-PS3="Restore #: "
+# Interactively choose a DB or ghost files archive to restore
+chooseFile () {
+	echo "Select DB or Ghost archive file to restore, or 'q' to quit"
+	PS3="Restore #: "
 
-select FILENAME in $BACKUP_LOCATION/*;
-do
-  case $FILENAME in
-        *)
-          echo "You picked $FILENAME"
-          restore $FILENAME
-          break;
-          ;;
-  esac
+	select FILENAME in $BACKUP_LOCATION/*;
+	do
+		[[ -z $FILENAME ]] && choice=$REPLY || choice=$FILENAME
+	  case $choice in
+	  		q|Q|exit) 
+				break;
+				;;
+	        *)
+				if [[ $choice =~ .*$DB_ARCHIVE_MATCH.* ]]
+				then
+					restoreDB $choice
+				elif [[ $choice =~ .*$GHOST_ARCHIVE_MATCH.* ]]
+				then
+					restoreGhost $choice
+				else
+					echo "unrecognised format - the file should be either a ghost files or db archive"
+				fi
+	          ;;
+	  esac
+	done
+}
+
+while getopts "i" opt; do
+	case $opt in
+		i)
+			chooseFile
+			;;
+	esac
 done
+
+
 
