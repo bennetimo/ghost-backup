b1ce95946459f8f927665614e77804619b1d80c3
Tim Bennett <bennetimo@gmail.com>
2015-12-15 23:35:12 +0000

    write mysql configuration to only user readble file

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,14 @@ if [[ $(crontab -l 2>/dev/null | egrep -c ghost-backup) -le 1 ]]; then
   
   # Add mysql env vars to the heredoc if that is the db being used
   if [ "$MYSQL_ENV_DB_CLIENT" == "mysql" ]; then 
-  	read -r -d '' MYSQL_ENVS <<-EOF
+  	cat <<-EOF>~/mysql.env
 	MYSQL_ENV_DB_CLIENT=$MYSQL_ENV_DB_CLIENT
 	MYSQL_ENV_MYSQL_USER=$MYSQL_ENV_MYSQL_USER
 	MYSQL_ENV_MYSQL_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
 	MYSQL_ENV_MYSQL_ROOT_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
 	EOF
+	MYSQL_ENVS=". ~/mysql.env; "
+	chmod 600 ~/mysql.env
   else
   	MYSQL_ENVS=""
   fi
@@ -20,8 +22,7 @@ if [[ $(crontab -l 2>/dev/null | egrep -c ghost-backup) -le 1 ]]; then
   	cat <<-EOF
 	BACKUP_LOCATION=$BACKUP_LOCATION
 	BACKUPS_RETAIN_LIMIT=$BACKUPS_RETAIN_LIMIT
-	$MYSQL_ENVS
-	$BACKUP_TIME ghost-backup >> $LOG_LOCATION 2>&1
+	$BACKUP_TIME $MYSQL_ENVS ghost-backup >> $LOG_LOCATION 2>&1
 	EOF
   } | crontab -
 fi
